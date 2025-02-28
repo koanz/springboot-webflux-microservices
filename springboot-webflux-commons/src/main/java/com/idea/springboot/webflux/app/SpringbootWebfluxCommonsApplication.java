782457853db0e4f2858d6d994f3a22a5505161c4
Cristian <cristiananzawa@gmail.com>
2025-02-28 11:57:52 -0300

    commons module added

---
+++ springboot-webflux-commons/src/main/java/com/idea/springboot/webflux/app/SpringbootWebfluxCommonsApplication.java
@@ -0,0 +1,13 @@
+package com.idea.springboot.webflux.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxCommonsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxCommonsApplication.class, args);
+	}
+
+}
---
+++ springboot-webflux-commons/src/test/java/com/idea/springboot/webflux/app/SpringbootWebfluxCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.springboot.webflux.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
