0967f4ac9874ba25cc36214e4bf282e843bfdaf1
Cristian <cristiananzawa@gmail.com>
2025-02-28 18:34:08 -0300

    global exception handlers created

--- springboot-webflux-client/src/main/java/com/idea/springboot/webflux/app/exceptions/GlobalExceptionHandler.java
+++ springboot-webflux-client/src/main/java/com/idea/springboot/webflux/app/exceptions/GlobalExceptionHandler.java
@@ -1,2 +1,63 @@
-package com.idea.springboot.webflux.app.exceptions;public class GlobalExceptionHandler {
+package com.idea.springboot.webflux.app.exceptions;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.support.WebExchangeBindException;
+import reactor.core.publisher.Mono;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(WebExchangeBindException.class)
+    public Mono<ResponseEntity<Map<String, Object>>> handleValidationExceptions(WebExchangeBindException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        List<String> errors = ex.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        errorResponse.put("errors", errors);
+        errorResponse.put("trace_id", UUID.randomUUID().toString());
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("status", HttpStatus.BAD_REQUEST.value());
+
+        return Mono.just(ResponseEntity.badRequest().body(errorResponse));
+    }
+
+    @ExceptionHandler(CategoryNotFountByIdException.class)
+    public ResponseEntity<Map<String, Object>> handleCategoryNotFountException(CategoryNotFountByIdException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("trace_id", UUID.randomUUID().toString());
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleProductNotFoundException(ProductNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("trace_id", UUID.randomUUID().toString());
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+
+    @ExceptionHandler(CustomNotFoundException.class)
+    public ResponseEntity<Map<String, Object>> handleCustomErrorResponseException(CustomNotFoundException ex) {
+        Map<String, Object> errorResponse = new HashMap<>();
+        errorResponse.put("message", ex.getMessage());
+        errorResponse.put("trace_id", UUID.randomUUID().toString());
+        errorResponse.put("timestamp", new Date());
+        errorResponse.put("status", HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
 }
