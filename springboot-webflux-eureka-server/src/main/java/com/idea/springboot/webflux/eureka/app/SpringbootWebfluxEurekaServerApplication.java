4affa75d1fb921a201148b3cbe2ae899d9ca3c9a
Cristian <cristiananzawa@gmail.com>
2025-03-01 14:18:20 -0300

    msvc eureka server

---
+++ springboot-webflux-eureka-server/src/main/java/com/idea/springboot/webflux/eureka/app/SpringbootWebfluxEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.idea.springboot.webflux.eureka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebfluxEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebfluxEurekaServerApplication.class, args);
+	}
+
+}
---
+++ springboot-webflux-eureka-server/src/test/java/com/idea/springboot/webflux/eureka/app/SpringbootWebfluxEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idea.springboot.webflux.eureka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebfluxEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
